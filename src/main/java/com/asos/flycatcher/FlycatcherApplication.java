a62a1f8e1e6a9b9e189f2418a40a420441fdaa8b
Osman Asaf <osmaan.tatar@gmail.com>
2023-07-10 23:41:38 +0300

    init

---
+++ src/main/java/com/asos/flycatcher/FlycatcherApplication.java
@@ -0,0 +1,13 @@
+package com.asos.flycatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlycatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlycatcherApplication.class, args);
+	}
+
+}
